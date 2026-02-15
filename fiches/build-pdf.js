444563aee82fd0ca1cc3febfb863abfe288101db
Hugo Gresse <hugo.gresse@gmail.com>
2026-02-15 15:52:54 +0100

    Fix pdf path

--- fiches/build-pdf.js
+++ fiches/build-pdf.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 
 const PORT = 4322;
-const BASE = '/open-formation-civique';
+const BASE = '/';
 const PDF_FILENAME = 'formation-civique';
 
 function startPreviewServer() {
